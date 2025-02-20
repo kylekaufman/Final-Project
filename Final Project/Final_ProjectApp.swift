afa2272ea3f4e264a3f209bb6799292a74e434b6
kylekaufman <ktk33@case.edu>
2025-02-20 11:05:39 -0500

    Initial Commit

---
+++ Final Project/Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_ProjectApp.swift
+//  Final Project
+//
+//  Created by Kyle Kaufman on 2/20/25.
+//
+
+import SwiftUI
+
+@main
+struct Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
